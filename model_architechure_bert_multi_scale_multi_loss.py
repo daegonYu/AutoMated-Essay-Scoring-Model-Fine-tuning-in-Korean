04fcbdf465a0f4a400c43dc13a21f11f290df82a
daegonYu <ydg5025@naver.com>
2023-03-09 17:12:11 +0900

    cosinesimilarity Loss using torch

--- model_architechure_bert_multi_scale_multi_loss.py
+++ model_architechure_bert_multi_scale_multi_loss.py
@@ -26,14 +26,21 @@ import numpy as np
 import os.path
 
 
+# def sim(y,yhat):
+#     e = torch.tensor(1e-8)
+#     m = torch.pow(torch.pow(y,2).sum(),0.5) * torch.pow(torch.pow(yhat,2).sum(),0.5)
+#     similarity = torch.sum(y*yhat) / torch.max(torch.tensor([m,e]))
+#     # cos = nn.CosineSimilarity(dim=0, eps=1e-8)      # batchsize만큼의 점수리스트가 들어옴으로 dim=0
+#     # loss = 1- cos(y,yhat)
+#     loss = 1 - similarity
+#     return loss            # 논문보니 평균내지 않는다.
+
+# torch 이용해서 간단히 구하기(같은 식)
 def sim(y,yhat):
-    e = torch.tensor(1e-8)
-    m = torch.pow(torch.pow(y,2).sum(),0.5) * torch.pow(torch.pow(yhat,2).sum(),0.5)
-    similarity = torch.sum(y*yhat) / torch.max(torch.tensor([m,e]))
-    # cos = nn.CosineSimilarity(dim=0, eps=1e-8)      # batchsize만큼의 점수리스트가 들어옴으로 dim=0
-    # loss = 1- cos(y,yhat)
-    loss = 1 - similarity
-    return loss            # 논문보니 평균내지 않는다.
+    cos = F.cosine_similarity(y,yhat, dim=0)
+    loss = 1 - cos
+    return loss
+
 
 def mr_loss_func(pred,label):
     # 배치사이즈가 1이 되면 에러가 나온다.
@@ -216,18 +223,18 @@ class DocumentBertScoringModel():
         weight_decay = 0.005    # 논문 : 0.005
         
         # 옵티마이져 : RAdam
-        word_document_optimizer = torch.optim.RAdam(self.bert_regression_by_word_document.parameters(),lr=lr,weight_decay=weight_decay)
-        chunk_optimizer = torch.optim.RAdam(self.bert_regression_by_chunk.parameters(),lr=lr,weight_decay=weight_decay)
+        # word_document_optimizer = torch.optim.RAdam(self.bert_regression_by_word_document.parameters(),lr=lr,weight_decay=weight_decay)
+        # chunk_optimizer = torch.optim.RAdam(self.bert_regression_by_chunk.parameters(),lr=lr,weight_decay=weight_decay)
         
         # 논문 Adam
-        # word_document_optimizer = torch.optim.Adam(self.bert_regression_by_word_document.parameters(),lr=lr,weight_decay=weight_decay)
-        # chunk_optimizer = torch.optim.Adam(self.bert_regression_by_chunk.parameters(),lr=lr,weight_decay=weight_decay)
+        word_document_optimizer = torch.optim.Adam(self.bert_regression_by_word_document.parameters(),lr=lr,weight_decay=weight_decay)
+        chunk_optimizer = torch.optim.Adam(self.bert_regression_by_chunk.parameters(),lr=lr,weight_decay=weight_decay)
         
         # lr 스케줄러
-        # word_document_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=word_document_optimizer,
-        #                                 lr_lambda=lambda epoch: 0.95 ** epoch)
-        # chunk_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=chunk_optimizer,
-        #                                 lr_lambda=lambda epoch: 0.95 ** epoch)
+        word_document_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=word_document_optimizer,
+                                        lr_lambda=lambda epoch: 0.95 ** epoch)
+        chunk_scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=chunk_optimizer,
+                                        lr_lambda=lambda epoch: 0.95 ** epoch)
         
         correct_output = None
         # bigbird를 위한 맥스길이 변경
@@ -300,6 +307,10 @@ class DocumentBertScoringModel():
                 word_document_optimizer.zero_grad() # 기울기 초기화
                 chunk_optimizer.zero_grad()
             
+            # lr 스케줄러
+            word_document_scheduler.step()  # 학습률 업데이트
+            chunk_scheduler.step()
+            
             if epoch % 2 == 0 and test:        # 2 에폭마다 test 셋으로 성능 체크
                 print('epoch : {}'.format(epoch))
                 new_pearson, new_qwk = self.predict_for_regress(test)      # 여기서 txt쓰기 삭제, eval 모드로 변경됨
@@ -328,9 +339,6 @@ class DocumentBertScoringModel():
                             f.close()
                             break
 
-            # lr 스케줄러
-            # word_document_scheduler.step()  # 학습률 업데이트
-            # chunk_scheduler.step()
         
         # pearson_list와 qwk_list 저장
         pearson_list = np.array(pearson_list)
